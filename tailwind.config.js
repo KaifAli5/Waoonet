b0971c5ce6c88226c6c9159dfef5a7e107f7c65e
KaifAli5 <153009655+KaifAli5@users.noreply.github.com>
2025-08-12 16:54:53 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue-bg': '#0a2540',
+        'darker-blue-bg': '#071b2c',
+        'accent-purple': '#6b46c1',
+        'light-purple-button': '#9f7aea',
+        'highlight-blue': '#3182ce',
+      },
+    },
+  },
+  plugins: [],
+};
